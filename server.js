3fb3963cb8a088a0f835446700e62bdefbaae6e1
RhythmicRhythm <Okubadejorh@gmail.com>
2023-07-22 19:11:16 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,32 @@
+const Queue = require("bull");
+const Redis = require("ioredis");
+
+const redisConfig = {
+  port: 6379, 
+  host: "localhost", 
+  
+};
+
+const redisClient = new Redis(redisConfig);
+const queue = new Queue("countdown", { redis: redisClient });
+
+function countDown() {
+  let count = 10;
+  const interval = setInterval(() => {
+    console.log(count);
+    count--;
+    if (count === 0) {
+      clearInterval(interval);
+      console.log("Countdown completed!");
+    }
+  }, 10000); 
+}
+
+queue.process(async (job) => {
+  console.log("Countdown job started");
+  countDown();
+});
+
+queue.add({});
+
+console.log("Enqueued countdown job");
